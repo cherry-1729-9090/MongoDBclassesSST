3fe2edc349f9b52aaa871a14108f32f8de401eae
Devi Charan <145871325+cherry-1729-9090@users.noreply.github.com>
2024-07-05 11:31:53 +0530

    Created a post method for adding products using express

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
 const express = require('express');
 const mongoose = require('mongoose');
-mongoose
-    .connect("mongodb+srv://chitluridevicharan:1Ev4GXGXac8SsyCR@cluster0.qgrpql7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log("Error connecting to MongoDB", err);
-    });
-
-    const productSchema = new mongoose.Schema({
-        product_name : {
-            type : String,
-            required : true
-        },
-        product_price :{
-            type : String,
-            required : true
-        },
-
-        product_description : {
-            type : String,
-            required : true
-        },
-        inStock : {
-            type : Boolean,
-            required : true
-        },
-
-        category : {
-            type : String,
-            required : true
-        }
-
-    });
-
 const app = express();
 
-app.listen(8006,()=>{
-    console.log('Server is running on port 8006')
-})
+mongoose
+  .connect("mongodb+srv://chitluridevicharan:1Ev4GXGXac8SsyCR@cluster0.qgrpql7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
+const productSchema = new mongoose.Schema({
+  product_name: {
+    type: String,
+    required: true
+  },
+  product_price: {
+    type: String,
+    required: true
+  },
+  product_description: {
+    type: String,
+    required: true
+  },
+  inStock: {
+    type: Boolean,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  }
+});
 
+const Product = mongoose.model('Product', productSchema);
+
+// adding a product
+app.use(express.json());
+
+app.post('/api/products', async (req, res) => {
+  const body = req.body;
+
+  const product = await Product.create({
+    product_name: body.product_name,
+    product_price: body.product_price,
+    product_description: body.product_description,
+    inStock: body.inStock,
+    category: body.category
+  });
+
+  console.log(product);
+  return res.status(201).json({ message: "Product added successfully", product });
+  
+});
+
+app.listen(8006, () => {
+  console.log('Server is running on port 8006');
+});
